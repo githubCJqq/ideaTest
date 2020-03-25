3c4aab8a91a03e61b921ad558fffad79305005b3
CJgitee <691327314@qq.com>
2020-03-25 17:36:38 +0800

    把本地工程添加到git

--- Git_01_test/src/com/GitTest.java
+++ Git_01_test/src/com/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git----测试-----");
     }
+    public void test(){
+        System.out.println("水水水水水水水水水水水水水水水水水水");
+    }
 }
